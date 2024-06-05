8bd2a17071c0be1fba05f8d4dae878fd23c3e766
wjdguswn1203 <wjdguswn0954@gmail.com>
2024-06-05 06:59:40 +0000

    port add

--- server/app.js
+++ server/app.js
@@ -26,13 +26,15 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
 
 // DB CONNECT
 const connection = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    database: DB_NAME
+    database: DB_NAME,
+    port: process.env.DB_PORT
   });
 
 connection.connect(error => {
